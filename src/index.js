597d2148f6bafd2e0c2d891e3aefb9dfdfdecaf2
mahrokhnz <mahrokh.nz@gmail.com>
2023-07-14 14:53:09 +0330

    fix: route

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Laptop from './projects/laptop/index';
-import Layout from './App'
-import './index.css'
+import Laptop from "./projects/laptop/index";
+import Layout from "./App";
+import "./index.css";
 
 export default function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Layout />} />
-                <Route path='/my-laptop' element={<Laptop />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/react-js" element={<Layout />} />
+        <Route path="/my-laptop" element={<Laptop />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
