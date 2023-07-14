56b35b297ad6e50250263c9d58440bafe4276024
mahrokhnz <mahrokh.nz@gmail.com>
2023-07-14 13:55:20 +0330

    refactor: file structure and add react router

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import {Link} from "react-router-dom";
+
+function App() {
+  return (
+      <ul>
+          <li>
+              <Link to="/my-laptop">Laptop</Link>
+          </li>
+      </ul>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Laptop from './projects/laptop/index';
+import Layout from './App'
+
+export default function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />} />
+                <Route path='/my-laptop' element={<Laptop />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
